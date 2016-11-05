e2280688d2cf968d7f3f2ce9838e2c759233d994
mwbullen <mwbullen@gmail.com>
2016-11-05 11:06:47 -0400

    Initial commit, script to generate map strings

---
+++ MapTest/Assets/MapGeneration.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MapGeneration : MonoBehaviour {
+	public int rowSize = 200;
+	public int numberRows = 10;
+
+	public float waterChance = .1f;
+	public float mountainChance = .1f;
+
+	public string mapString;
+
+	public ArrayList mapDefinition = new ArrayList();
+
+	// Use this for initialization
+	void Start () {
+		UnityEngine.Debug.Log("test");
+
+		createMap ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void createMap() {
+		mapDefinition.Clear ();
+
+		for (int i = 0; i < (rowSize * numberRows); i ++) {
+			char c = '_';
+
+			//check for water
+			float tileCheckF = Random.Range (0f, 1f);
+
+			float tmpWaterChance = waterChance;
+
+			//check for adjacent tiles
+
+			if (getTileStringatPosition (i - 1) == "w") {
+				tmpWaterChance = tmpWaterChance * 2;
+			}
+
+			if (getTileStringatPosition (i - rowSize) == "w") {
+				tmpWaterChance = tmpWaterChance * 2;
+			}
+
+			//UnityEngine.Debug.Log(tileCheckF);
+			if (tileCheckF < tmpWaterChance) {
+				//water tile
+				c = 'w';
+			} 
+
+			mapDefinition.Add (c);
+			mapString = mapString + c;
+		}
+	}
+
+	string getTileStringatPosition(int position) {
+		if (position > 0) {
+			return mapDefinition [position].ToString();
+		} else {
+			return "";
+		}
+	}
+}
