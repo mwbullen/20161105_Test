98e83d0933884788e85f7efd4500e1a94e2c7991
mwbullen <mwbullen@gmail.com>
2016-11-08 12:23:37 -0500

    click to move ball, revised nav obstacle size

---
+++ MapTest/Assets/mouseInput.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouseInput : MonoBehaviour {
+	GameObject terrain;
+
+	public GameObject character;
+
+	// Use this for initialization
+	void Start () {
+		terrain = GameObject.FindGameObjectWithTag ("Ground");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			//Debug.Log("Click");
+
+			RaycastHit r = new RaycastHit();
+			Ray cameraRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+			Physics.Raycast (cameraRay, out r);
+
+			if (r.collider.gameObject == terrain) {
+				//Debug.Log (r.point);
+
+				character.SendMessage ("NavtoPoint", r.point);
+			}
+		}
+	}
+
+
+}
