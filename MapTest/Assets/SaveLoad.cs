ecc93c0a9dea29d61e0684d0e6ebfa2dd854c449
mwbullen <mwbullen@gmail.com>
2016-11-10 14:58:33 -0500

    changes

---
+++ MapTest/Assets/SaveLoad.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class SaveLoad : MonoBehaviour {
+	GameObject Tribe;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Save() {
+		Tribe = GameObject.FindGameObjectWithTag ("Tribe");
+
+		Debug.Log (Application.persistentDataPath);
+		BinaryFormatter bf = new BinaryFormatter ();
+		FileStream fs = File.Create (Application.persistentDataPath + "/" + "savegame.tst");
+
+		foreach (GameObject g in  GameObject.FindGameObjectsWithTag("Character")) {
+			
+
+			bf.Serialize(fs,  null);
+		}
+
+
+		fs.Close ();
+
+	}
+}
