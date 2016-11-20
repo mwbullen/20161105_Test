a0f68b956b1ed17b77a1d94022d8f482c00d39d6
mwbullen <Mike@new-host.home>
2016-11-20 18:00:47 -0500

    Added arrow key movement

--- MapTest/Assets/TribeMovement.cs
+++ MapTest/Assets/TribeMovement.cs
@@ -6,12 +6,16 @@ public class TribeMovement : MonoBehaviour {
 
 	//[HideInInspector] public GameObject currentTile;
 	public int currentTileID = 125;
+	[HideInInspector] public int rowSize;
+
 	GameObject gameControl;
 
 
 	// Use this for initialization
 	void Start () {
 		gameControl = GameObject.FindGameObjectWithTag ("GameControl");
+
+		rowSize = gameControl.GetComponent<MapGeneration> ().rowSize;
 
 		SpawnatLastTile ();
 	}
@@ -32,11 +36,44 @@ public class TribeMovement : MonoBehaviour {
 	public void MovetoTile(GameObject targetTile ) {		
 		PlayMakerFSM movementFsm = gameObject.GetComponent<PlayMakerFSM> ();
 
-		movementFsm.FsmVariables.GetFsmGameObject("targetTile").Value = targetTile;
-		movementFsm.SendEvent ("Move to Tile");
+		//need to confirm target tile is walkable
+		if (targetTile.GetComponent<TileInfo> ().Walkable) {
+			movementFsm.FsmVariables.GetFsmGameObject ("targetTile").Value = targetTile;
+			movementFsm.SendEvent ("Move to Tile");
 
-		currentTileID = targetTile.GetComponent<TileInfo> ().TileID;
+			currentTileID = targetTile.GetComponent<TileInfo> ().TileID;
 
-		gameObject.SendMessage ("updateTilesInRange");
+			gameObject.SendMessage ("updateTilesInRange");
+		}
+		}
+
+	// Directional movement
+
+	public void MoveUp() {
+		int targetTileID = currentTileID + rowSize;
+
+		MovetoTile (gameControl.GetComponent<MapGeneration> ().DisplayTile (targetTileID));
+
+	}
+
+	public void MoveDown() {
+		int targetTileID = currentTileID - rowSize;
+
+		MovetoTile (gameControl.GetComponent<MapGeneration> ().DisplayTile (targetTileID));
+
+	}
+
+	public void MoveLeft() {
+		int targetTileID = currentTileID -1;
+
+		MovetoTile (gameControl.GetComponent<MapGeneration> ().DisplayTile (targetTileID));
+
+	}
+
+	public void MoveRight() {
+		int targetTileID = currentTileID + 1;
+
+		MovetoTile (gameControl.GetComponent<MapGeneration> ().DisplayTile (targetTileID));
+
 	}
 }
