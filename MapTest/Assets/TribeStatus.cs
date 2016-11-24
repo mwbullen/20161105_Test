01e5b0b836f9e3b5a2dd19fd55b29041314ae231
mwbullen <Mike@new-host.home>
2016-11-24 14:38:21 -0500

    Add tribe status script, remove old scripts

---
+++ MapTest/Assets/TribeStatus.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TribeStatus : MonoBehaviour {
+
+	public int TribeCount;
+
+	public float dailyFoodNeed;
+	public float foodStorage;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
