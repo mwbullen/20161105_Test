093a5fde4a6e3651d7027707d20b01f3e5f4b141
mwbullen <Mike@new-host.home>
2016-11-25 10:23:03 -0500

    Load saved tribeinfo

--- MapTest/Assets/Scripts/SaveLoad.cs
+++ MapTest/Assets/Scripts/SaveLoad.cs
@@ -6,6 +6,7 @@ using System.Runtime.Serialization.Formatters.Binary;
 public class SaveLoad : MonoBehaviour {
 	GameObject Tribe;
 
+	string SaveTribeInfoFileName = "tribeSave.gam";
 	// Use this for initialization
 	void Start () {
 		Tribe = GameObject.FindGameObjectWithTag ("Tribe");
@@ -21,12 +22,26 @@ public class SaveLoad : MonoBehaviour {
 		BinaryFormatter bf = new BinaryFormatter ();
 
 		Debug.Log (Application.persistentDataPath);
-		FileStream fs = File.Create (Application.persistentDataPath + "/" + "savegame.tst");
+		FileStream fs = File.Create (Application.persistentDataPath + "/" + SaveTribeInfoFileName);
 
 		bf.Serialize(fs,Tribe.GetComponent<TribeStatus>().tribeInfo);
 			
 		fs.Flush ();
 		fs.Close ();
 
+	}
+
+	public TribeInfo LoadSavedTribeInfo() {
+		if (File.Exists (Application.persistentDataPath + "/" + SaveTribeInfoFileName)) {
+			BinaryFormatter bf = new BinaryFormatter ();
+
+			FileStream fs = File.Create (Application.persistentDataPath + "/" + SaveTribeInfoFileName);
+
+			TribeInfo savedTribeInfo = (TribeInfo) bf.Deserialize (fs);
+
+			return savedTribeInfo;
+		}
+
+		return null;
 	}
 }
